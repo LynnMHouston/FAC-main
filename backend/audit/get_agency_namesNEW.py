dfa058e44a36b82c658ed1df53b019ad894e2c58
LynnMHouston <lynn.houston@gsa.gov>
2024-12-18 17:58:39 -0500

    Add files via upload

--- backend/audit/get_agency_namesNEW.py
+++ backend/audit/get_agency_namesNEW.py
@@ -1,11 +1,10 @@
 from pathlib import Path
 import csv
 import requests
+import json
 import os
 
-
 CSV_URL = "https://sam.gov/api/prod/fileextractservices/v1/api/download/Assistance%20Listings/datagov/AssistanceListings_DataGov_PUBLIC_CURRENT.csv?privacy=Public"
-
 
 def download_latest_csv(download_path):
     """
@@ -20,30 +19,23 @@ def download_latest_csv(download_path):
     else:
         raise Exception(f"Failed to download CSV. Status code: {response.status_code}")
 
-
 def get_agency_names(csv_file):
     """
     From a CSV of agency data, return a dictionary with the agency codes as
-    keys and the names as values, in order:
-
-        {
-            "00": "None",
-            "01": "African Development Foundation",
-            "02": "Agency for International Development",
-            …
-        }
+    keys and the names as values.
 
     Scrap rows with non-numeric agency nums and rows with empty agency names.
     """
-    with open(csv_file, "r", newline="", encoding="UTF-8") as file:
-        reader = csv.reader(file)
-        # Skip the header row if there is one
-        next(reader, None)
-        sorted_agencies = sorted(reader, key=lambda x: x[0])
-
-    valid_agencies = filter(lambda r: r[0].isnumeric() and r[1] != "", sorted_agencies)
-    return {row[0]: row[1] for row in valid_agencies}
-
+    with open(csv_file, "r", newline="", encoding="Windows-1252") as file:
+        reader = csv.DictReader(file)
+        
+        # Extract 'Program Number' as the key and 'Federal Agency (030)' as the value
+        valid_agencies = {
+            row["Program Number"]: row["Federal Agency (030)"]
+            for row in reader
+            if row["Program Number"].replace(".", "").isnumeric() and row["Federal Agency (030)"].strip()
+        }
+    return valid_agencies
 
 def get_audit_info_lists(name):
     """
@@ -67,7 +59,6 @@ def get_audit_info_lists(name):
     jobj = json.loads(jsonfile.read_text(encoding="UTF-8"))
 
     return [info for info in jobj[name] if not info.get("historical_only") is True]
-
 
 if __name__ == "__main__":
     # Define the path for the downloaded CSV
